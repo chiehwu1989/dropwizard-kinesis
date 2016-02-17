70a5f97a9b79d9e97524572befd905228fdfacce
Randy Pensinger <rspensinger@gmail.com>
2016-02-17 02:25:35 -0800

    trying to notify on releases

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "${TRAVIS_TAG}"
+
+if [[ "${TRAVIS_TAG}" == "dropwizard-kinesis-*" ]] ; then
+    curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
+fi
+
 mvn -B jacoco:report coveralls:report
