e59c28d79d674bf85868b51bdc4f4cfba0ae3ac7
Randy Pensinger <rspensinger@gmail.com>
2016-02-17 02:42:16 -0800

    proper release version, should have release notifications now

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "${TRAVIS_TAG}"
-
-if [[ "${TRAVIS_TAG}" == "dropwizard-kinesis-*" ]] ; then
+if [[ $TRAVIS_TAG == dropwizard-kinesis* ]] ; then
     curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
 fi
 
