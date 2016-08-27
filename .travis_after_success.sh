e3d930b93ebb1b3f22ce20075e6377e67643f49a
Randy Pensinger <rspensinger@gmail.com>
2016-08-26 23:48:17 -0700

    should not be setting version here

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -4,6 +4,5 @@ mvn -B jacoco:report coveralls:report
 
 version_match=`perl -e "print '$TRAVIS_TAG' =~ /^dropwizard-kinesis-\d+\.\d+\.\d+\.\d+$/"`
 if [[ "$version_match" == "1" ]]; then
-    mvn versions:set -DnewVersion=$TRAVIS_TAG
     mvn clean deploy --settings .travis/release-settings.xml -DskipTests=true -B
 fi
