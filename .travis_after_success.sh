d351d300ccf73c762fc4769ff2c031c8d2c13718
Randy Pensinger <rspensinger@gmail.com>
2016-08-27 00:13:30 -0700

    slack messaging

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -5,4 +5,5 @@ mvn -B jacoco:report coveralls:report
 version_match=`perl -e "print '$TRAVIS_TAG' =~ /^dropwizard-kinesis-\d+\.\d+\.\d+\.\d+$/"`
 if [[ "$version_match" == "1" ]]; then
     mvn clean deploy --settings .travis/release-settings.xml -DskipTests=true -B
+    curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
 fi
